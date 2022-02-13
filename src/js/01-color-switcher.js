0e25fa83f2611127c55d3b906271def33516ebec
Oleksandr Pozden <o.pozden@admins-MacBook-Pro-2.local>
2022-02-13 14:05:10 +0200

    task 1 is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,32 @@
 const hrefs = {
     buttonStart: document.querySelector('[data-start]'),
     buttonStop: document.querySelector('[data-stop]'),
+    disableStartButton() {
+        this.buttonStart.disabled = true;
+    },
+    enableStartButton() {
+        this.buttonStart.disabled = false;
+    }
 }
-console.log(hrefs)
+const INTERVAL_TIME = 1000;
+let intervalId = null;
+hrefs.buttonStart.addEventListener('click', onClickStart);
+hrefs.buttonStop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+    hrefs.disableStartButton()
+    intervalId = setInterval(changeBackgroundColor, INTERVAL_TIME);
+}
+
+function onClickStop() {
+    hrefs.enableStartButton()
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
